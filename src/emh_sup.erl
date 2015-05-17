b29c2b84cbdc58a994a30aa665d6e385e3967a1c
Caio Ceccon <contact@caioceccon.com>
2015-05-17 04:15:05 +0200

    Add emh supervisor

---
+++ src/emh_sup.erl
@@ -0,0 +1,18 @@
+-module(emh_sup).
+-behaviour(supervisor).
+ 
+-export([start_link/0, init/1]).
+ 
+start_link() ->
+supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+ 
+init([]) ->
+    {ok, {{one_for_one, 60, 3600},
+    [
+     {emh_hubservsup,
+      {emh_hubserv_sup, start_link, []},
+      permanent, 1000, worker, [emh_hubserv]},
+     {emh_clientservsup,
+      {emh_clientserv_sup, start_link, []},
+      permanent, 1000, worker, [emh_clientserv]}
+    ]}}.
