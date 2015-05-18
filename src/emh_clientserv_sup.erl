a471f1d00f089c9857ae77a175fa653b8f57df2e
Caio Ceccon <contact@caioceccon.com>
2015-05-19 00:38:46 +0200

    Use port passed by env

--- src/emh_clientserv_sup.erl
+++ src/emh_clientserv_sup.erl
@@ -14,7 +14,7 @@ init([]) ->
         end,
 
     %% Set the socket into {active_once} mode.
-    {ok, ListenSocket} = gen_tcp:listen(9023, [binary, {active,once}, {packet,line}]),
+    {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {active,once}, {packet,line}]),
     spawn_link(fun empty_listeners/0),
 
     {ok, {{simple_one_for_one, 60, 3600},
